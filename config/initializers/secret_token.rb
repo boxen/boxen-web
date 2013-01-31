c19eadf2c8d7e7f453b4b2a0c97ea9c94e70f65a
Will Farrington <wfarr@github.com>
2013-01-31 13:41:47 -0800

    Move session secret to env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Boxen::Application.config.secret_token = ENV['SECRET_TOKEN']
+abort "You must set a SECRET_TOKEN environment variable!" unless Boxen::Application.config.secret_token = ENV['SECRET_TOKEN']
