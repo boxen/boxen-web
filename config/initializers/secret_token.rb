3eb4bd7a6db4930b64f25d6b9629c8268f6ef64f
Will Farrington <wfarr@github.com>
2013-01-31 13:40:23 -0800

    zomg don't use a global secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Boxen::Application.config.secret_token = '2390587737253cc35a9111d34aa65d5a4f4cc761e4befb41454c1f04a581a92d9c6ac443430fdeae1737b24f0e0ce9f2837f3095639ac25e524aa01295db09f2'
+Boxen::Application.config.secret_token = ENV['SECRET_TOKEN']
