8f25d09ca58435a8a24aeef9426d15c042d005c2
Jacob Bednarz <jacob.bednarz@gmail.com>
2017-02-22 17:46:09 +1100

    Remove `active_record.auto_explain_threshold_in_seconds`

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 Boxen::Application.configure do
   config.action_controller.perform_caching               = false
   config.action_dispatch.best_standards_support          = :builtin
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.active_record.mass_assignment_sanitizer         = :strict
   config.active_support.deprecation                      = :log
   config.assets.compress                                 = false
